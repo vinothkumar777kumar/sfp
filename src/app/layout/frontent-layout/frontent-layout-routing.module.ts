7dbcde9b8ddabef8bd263adbaabd8566b3f58281
vinothkumar777kumar <39370102+vinothkumar777kumar@users.noreply.github.com>
2021-06-09 18:25:10 +0530

    sfp_changes_962021623

--- src/app/layout/frontent-layout/frontent-layout-routing.module.ts
+++ src/app/layout/frontent-layout/frontent-layout-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from 'src/app/pages/front-end/home/home.component';
+import { AboutComponent } from 'src/app/pages/front-end/about/about.component';
+import { CoursesComponent } from 'src/app/pages/front-end/courses/courses.component';
+import { ContactComponent } from 'src/app/pages/front-end/contact/contact.component';
 
 
 export const FrontentLayoutRoutingModule: Routes = [
    { path: 'home',component:HomeComponent},
+   { path: 'about',component:AboutComponent},
+   { path: 'courses',component:CoursesComponent},
+   { path: 'contact',component:ContactComponent}
 ];
 
 // @NgModule({
--- src/app/layout/frontent-layout/frontent-layout.module.ts
+++ src/app/layout/frontent-layout/frontent-layout.module.ts
@@ -6,11 +6,15 @@ import { FrontentLayoutComponent } from './frontent-layout.component';
 import { FrontendHeaderComponent } from 'src/app/component/frontend-header/frontend-header.component';
 import { FrontendFooterComponent } from 'src/app/component/frontend-footer/frontend-footer.component';
 import { HomeComponent } from 'src/app/pages/front-end/home/home.component';
+import { AboutComponent } from 'src/app/pages/front-end/about/about.component';
+import { CoursesComponent } from 'src/app/pages/front-end/courses/courses.component';
+import { ContactComponent } from 'src/app/pages/front-end/contact/contact.component';
 
 
 
 @NgModule({
-  declarations: [FrontentLayoutComponent,FrontendHeaderComponent,FrontendFooterComponent,HomeComponent],
+  declarations: [FrontentLayoutComponent,FrontendHeaderComponent,FrontendFooterComponent,HomeComponent,
+  AboutComponent,CoursesComponent,ContactComponent],
   imports: [
     CommonModule,RouterModule.forChild(FrontentLayoutRoutingModule)
   ]
